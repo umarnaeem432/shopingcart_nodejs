44b82e50e2552dbfa04cf77fde724e2c32de5de9
visadbio <zain@visadb.io>
2020-01-05 20:19:36 +0500

    Connected To Remote Database

--- server/utils/database.js
+++ server/utils/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'gct165201b' , {dialect: 'mysql', host: 'localhost'});
-
+// const sequelize = new Sequelize('node-complete', 'root', 'gct165201b' , {dialect: 'mysql', host: 'localhost'});
+const sequelize = new Sequelize('TIDvjfR2nf', 'TIDvjfR2nf', 'VCvdKABS1T' , {dialect: 'mysql', host: 'remotemysql.com'});
 module.exports = sequelize;
